35a73a0dbca03de4e62e0ffbf4c721de6c1882d3
tejakiransirivella <tejakiran2013@gmail.com>
2025-04-24 18:59:06 -0400

    fixed cv issue with hold out as backup

--- backend/meta_learning/best_candidate_run.py
+++ backend/meta_learning/best_candidate_run.py
@@ -1,6 +1,6 @@
 class BestCandidateRun:
 
-    def __init__(self,dataset_id:int, best_config:dict, val_score:float,test_score:float):
+    def __init__(self,dataset_id:int=None, best_config:dict=None, val_score:float=None,test_score:float=None):
         self.dataset_id = dataset_id
         self.best_config = best_config
         self.val_score = val_score
--- backend/pipelines/BasePipeline.py
+++ backend/pipelines/BasePipeline.py
@@ -1,5 +1,6 @@
+from collections import Counter
 from ConfigSpace import Configuration
-from sklearn.model_selection import cross_val_score
+from sklearn.model_selection import cross_val_score, train_test_split
 import numpy as np
 
 class BasePipeline:
@@ -10,8 +11,18 @@ class BasePipeline:
         """
         try:
             model = self.get_model_for_config(config,budget,seed)
-            scores = cross_val_score(model, X_train, y_train, cv=5)
-            return 1-np.mean(scores)
+            label_counts = Counter(y_train)
+            min_class_count = min(label_counts.values())    
+            if min_class_count < 5:
+                print(f"Class imbalance detected. Minimum class count: {min_class_count}")
+                print("Using 80/20 holdout split for training and validation.")
+                X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.2, random_state=seed)
+                model.fit(X_train, y_train)
+                val_score = model.score(X_val, y_val)
+                return 1-val_score
+            else:
+                scores = cross_val_score(model, X_train, y_train, cv=5)
+                return 1-np.mean(scores)
         except Exception as e:
             print(f"Training failed for config {config}")
             print("Error: ", e)
